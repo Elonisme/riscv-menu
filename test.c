1fa0e5ed2517e806dfd4c46347aa2de9c5fcc3fa
Elon Li <87638888+Elonisme@users.noreply.github.com>
2023-11-11 17:28:29 +0800

    Update test.c

--- test.c
+++ test.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include "menu.h"
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define FONTSIZE 10
 int PrintMenuOS()
 {
-    int i, j;
-    char data_M[FONTSIZE][FONTSIZE] =
+  int i, j;
+  char data_M[FONTSIZE][FONTSIZE] =
     {
         "          ",
         "  *    *  ",
@@ -117,6 +121,29 @@ int PrintMenuOS()
     return 0;
 }
 
+int CWrite(void){
+  char s[]="hello, world\n";
+	write(1,s,13);
+	return 0;
+  return 0;
+}
+
+int WriteAsm(void){
+      char s[] = "hello, world\n";
+
+    __asm__ volatile(
+		    "li a2, 13\n"
+		    "li a0, 1\n"
+		    "mv a1, %[str]\n"
+	    	    "li a7, 64\n"
+		    "ecall \n"
+		    :
+		    : [str] "r" (s)
+    );
+
+    return 0;
+}
+
 int Quit(int argc, char *argv[])
 {
     /* add XXX clean ops */
@@ -124,37 +151,104 @@ int Quit(int argc, char *argv[])
 
 int Time(int argc, char *argv[])
 {
-    time_t tt;
-    struct tm *t;
-    tt = time(NULL);
-    t = localtime(&tt);
-    printf("time:%d:%d:%d:%d:%d:%d\n",t->tm_year+1900, t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) == 0) {
+        time_t current_time = tv.tv_sec;
+        struct tm *tm_info = localtime(&current_time);
+
+        char buffer[20];
+        strftime(buffer, sizeof(buffer), "%Y:%m:%d %H:%M:%S", tm_info);
+        printf("Current time: %s\n", buffer);
+    } else {
+        perror("gettimeofday");
+    }
     return 0;
 }
 
 int TimeAsm(int argc, char *argv[])
 {
-    time_t tt;
-    struct tm *t;
-    asm volatile(
-        "mov $0,%%ebx\n\t"
-        "mov $0xd,%%eax\n\t" 
-        "int $0x80\n\t" 
-        "mov %%eax,%0\n\t"  
-        : "=m" (tt) 
-    );
-    t = localtime(&tt);
-    printf("time:%d:%d:%d:%d:%d:%d\n",t->tm_year+1900, t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-    return 0;
+  time_t tt;
+  struct timeval tv;
+  struct tm *t;
+
+  #if 0
+      gettimeofday(&tv, NULL);
+  #else
+  asm volatile(
+    "li a7, 169\n\t"
+    "li a0, 0\n\t"
+    "ecall\n\t"
+    "mv %0, a0\n\t"
+    : "=r" (tv.tv_sec)
+   );
+  #endif
+
+  tt = tv.tv_sec;
+  t = localtime(&tt);
+
+  printf("time:%d:%d:%d:%d:%d:%d\n",t->tm_year+1900, t->tm_mon, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+  return 0;
 }
+
+int ForkProcess(int argc, char *argv[]){
+  int pid;
+  pid = fork();
+  if(pid < 0){
+    fprintf(stderr, "Fork Failed!");
+    exit(-1);
+  }
+  else if (pid == 0){
+    printf("This is Child Process!\n");
+  }
+  else {
+    printf("This is Parent Process!\n");
+    wait(NULL);
+    printf("Child Complete!\n");
+  }
+
+  return 0;
+}
+
+
+int Exec(int argc, char *argv[])
+{
+	int pid;
+	/* fork another process */
+	pid = fork();
+	if (pid < 0)
+	{
+		/* error occurred */
+		fprintf(stderr,"Fork Failed!");
+		exit(-1);
+	}
+	else if (pid == 0)
+	{
+		/*	 child process 	*/
+    	printf("This is Child Process!\n");
+		execlp("/hello","hello",NULL);
+	}
+	else
+	{
+		/* 	parent process	 */
+    	printf("This is Parent Process!\n");
+		/* parent will wait for the child to complete*/
+		wait(NULL);
+		printf("Child Complete!\n");
+	}
+}
+
+
 int main()
 {
     PrintMenuOS();
     SetPrompt("MenuOS>>");
-    MenuConfig("version","MenuOS V1.0(Based on Linux 3.18.6)",NULL);
+    MenuConfig("version","MenuOs V2.0(Based on Linux 5.19.16)",NULL);
     MenuConfig("quit","Quit from MenuOS",Quit);
     MenuConfig("time","Show System Time",Time);
     MenuConfig("time-asm","Show System Time(asm)",TimeAsm);
+    MenuConfig("write", "Write hello world", CWrite);
+    MenuConfig("write-asm", "Write hello world(asm)", WriteAsm);
+    MenuConfig("fork-new", "fork new process", ForkProcess);
+    MenuConfig("exec", "execve new process", Exec);
     ExecuteMenu();
 }
-
